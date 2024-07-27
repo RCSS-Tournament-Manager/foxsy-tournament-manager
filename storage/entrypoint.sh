6366015236af442d3fd73f9063ff32a4f8061573
nader.zare <nader.zare88@gmail.com>
2024-07-26 22:08:53 -0300

    remove storage

--- storage/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Use shell parameter expansion to set defaults
-: "${DATA_DIR:=/app/data}"
-: "${LOG_DIR:=/app/logs}"
-: "${API_KEY:=api-key}"
-
-cd app
-exec python -m main --data-dir "$DATA_DIR" --log-dir "$LOG_DIR" --api-key "$API_KEY" "$@"
