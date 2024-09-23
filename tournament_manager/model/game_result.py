08a1e43122a71d3528274b13bd1b610d6e353d2a
Alireza SadraiiRad <sadraiiali@gmail.com>
2024-09-23 23:16:10 +0200

    remove old app

--- tournament_manager/model/game_result.py
+++
@@ -1,9 +0,0 @@
-from typing import Optional
-from sqlmodel import Field, SQLModel
-
-
-class GameResult(SQLModel, table=True):
-    game_id: Optional[int] = Field(default=None, foreign_key="game.id", primary_key=True)
-    team_left_score: int
-    team_right_score: int
-    winner_id: Optional[int] = Field(default=None, foreign_key="team.id")
