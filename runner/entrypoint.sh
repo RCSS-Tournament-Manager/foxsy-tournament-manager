892d97035c24cb16a16cd22dd4dda9a8bda96446
naderzare <nader.zare88@gmail.com>
2024-09-25 20:33:33 -0300

    clean entrypoint developed by alireza

--- runner/entrypoint.sh
+++ runner/entrypoint.sh
@@ -29,12 +29,30 @@
 
 cd app
 
-exec python -m main --data-dir "$DATA_DIR" --log-dir "$LOG_DIR" --api-key "$API_KEY" --max-games-count "$MAX_GAMES_COUNT" \
-    --use-fast-api "$USE_FAST_API" --fast-api-port "$FAST_API_PORT" ----fast-api-ip "$FAST_API_IP" \
-    --use-rabbitmq "$USE_RABBITMQ" --rabbitmq-host "$RABBITMQ_HOST" --rabbitmq-port "$RABBITMQ_PORT" \
-    --rabbitmq-username "$RABBITMQ_USERNAME" --rabbitmq-password "$RABBITMQ_PASSWORD" \
-    --connect-to-tournament-manager "$CONNECT_TO_TOURNAMENT_MANAGER" --tournament-manager-ip "$TOURNAMENT_MANAGER_IP" --tournament-manager-port "$TOURNAMENT_MANAGER_PORT" --tournament-manager-api-key "$TOURNAMENT_MANAGER_API_KEY" \
-    --use-minio "$USE_MINIO" --minio-endpoint "$MINIO_ENDPOINT" --minio-access-key "$MINIO_ACCESS_KEY" --minio-secret-key "$MINIO_SECRET_KEY" \
-    --server-bucket-name "$SERVER_BUCKET_NAME" --base-team-bucket-name "$BASE_TEAM_BUCKET_NAME" --team-config-bucket-name "$TEAM_CONFIG_BUCKET_NAME" \
-    --game-log-bucket-name "$GAME_LOG_BUCKET_NAME" --to-runner-queue "$TO_RUNNER_QUEUE"
+exec python -m main \
+    --data-dir "$DATA_DIR" \
+    --log-dir "$LOG_DIR" \
+    --api-key "$API_KEY" \
+    --max-games-count "$MAX_GAMES_COUNT" \
+    --use-fast-api "$USE_FAST_API" \
+    --fast-api-port "$FAST_API_PORT" \
+    ----fast-api-ip "$FAST_API_IP" \
+    --use-rabbitmq "$USE_RABBITMQ" \
+    --rabbitmq-host "$RABBITMQ_HOST" \
+    --rabbitmq-port "$RABBITMQ_PORT" \
+    --rabbitmq-username "$RABBITMQ_USERNAME" \
+    --rabbitmq-password "$RABBITMQ_PASSWORD" \
+    --connect-to-tournament-manager "$CONNECT_TO_TOURNAMENT_MANAGER" \
+    --tournament-manager-ip "$TOURNAMENT_MANAGER_IP" \
+    --tournament-manager-port "$TOURNAMENT_MANAGER_PORT" \
+    --tournament-manager-api-key "$TOURNAMENT_MANAGER_API_KEY" \
+    --use-minio "$USE_MINIO" \
+    --minio-endpoint "$MINIO_ENDPOINT" \
+    --minio-access-key "$MINIO_ACCESS_KEY" \
+    --minio-secret-key "$MINIO_SECRET_KEY" \
+    --server-bucket-name "$SERVER_BUCKET_NAME" \
+    --base-team-bucket-name "$BASE_TEAM_BUCKET_NAME" \
+    --team-config-bucket-name "$TEAM_CONFIG_BUCKET_NAME" \
+    --game-log-bucket-name "$GAME_LOG_BUCKET_NAME" \
+    --to-runner-queue "$TO_RUNNER_QUEUE"
 
