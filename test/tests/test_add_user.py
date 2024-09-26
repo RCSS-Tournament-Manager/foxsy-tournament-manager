f33c70fd59501ea2deab005652b6f0bbbea6ceaf
Alireza SadraiiRad <sadraiiali@gmail.com>
2024-09-26 20:57:30 +0200

    add integration testing project

--- test/tests/test_add_user.py
+++ test/tests/test_add_user.py
@@ -13,20 +13,23 @@ def test_add_user():
     # Get all users and check if the user exists
     response = get(f"user/get_all")
     assert response.status_code == 200
-    users = response.json()["obj"]["users"]
+    users = response.json()["users"]
     added_user = next((user for user in users if user["user_name"] == "test_user1"), None)
     assert added_user is not None
+    print(added_user)
 
     # Remove the user
     user_id = added_user["user_id"]
     remove_data = {"user_code": "test_test", "user_id": user_id}
-    response = delete(f"user", data=remove_data)
+    response = delete(f"user/{added_user['user_id']}")
     assert response.status_code == 200
     assert "success" in response.json().keys() and response.json()["success"] == True
 
     # Verify the user has been removed
     response = get(f"user/get_all")
     assert response.status_code == 200
-    users = response.json()["obj"]["users"]
+    users = response.json()["users"]
     removed_user = next((user for user in users if user["user_name"] == "test_user1"), None)
     assert removed_user is None
+
+
