721fad9006d56932b74f3310446b4917c5855fb0
MROA <43232848+mroa4@users.noreply.github.com>
2024-10-16 13:13:31 -0300

    Fix requested_command not updating and add debug logger

--- tournament_manager2/app/managers/runner_manager.py
+++ tournament_manager2/app/managers/runner_manager.py
@@ -23,6 +23,25 @@ class RunnerManager:
         self.logger.info('RunnerManager created')
         self.db_session = db_session
 
+    async def get_runner_model(self, runner_id: int) -> Optional[RunnerModel]:
+        self.logger.info(f"get_runner_model: {runner_id}")
+        try:
+            stmt = select(RunnerModel).where(RunnerModel.id == runner_id)
+            result = await self.db_session.execute(stmt)
+            runner = result.scalars().first()
+
+            if not runner:
+                self.logger.warning(f"Runner with id {runner_id} not found")
+                return None
+            
+            return runner
+        except SQLAlchemyError as e:
+            self.logger.error(f"Database error in get_runner_model: {e}")
+            return None
+        except Exception as e:
+            self.logger.error(f"Unexpected error in get_runner_model: {e}")
+            return None
+
     async def get_runner(self, runner_id: int) -> Union[GetRunnerResponseMessage, ResponseMessage]:
         self.logger.info(f"get_runner: {runner_id}")
         try:
@@ -65,7 +84,8 @@ class RunnerManager:
                     end_time=runner.end_time,
                     status=runner.status,
                     address=runner.address,
-                    available_games_count=runner.available_games_count
+                    available_games_count=runner.available_games_count,
+                    requested_command=runner.requested_command
                 ))
 
             return GetAllRunnersResponseMessage(runners=runners_list)
@@ -106,6 +126,7 @@ class RunnerManager:
         self.logger.info(f"handle_game_started: {json}")
         try:
             # Retrieve Runner
+            # self.logger.debug("Retrieving runner details...")
             stmt_runner = select(RunnerModel).options(selectinload(RunnerModel.games)).where(RunnerModel.id == json.runner_id)
             result_runner = await self.db_session.execute(stmt_runner)
             runner = result_runner.scalars().first()
@@ -119,7 +140,13 @@ class RunnerManager:
                 return ResponseMessage(success=False, error="Failed to start game")
 
             # Retrieve Game
-            stmt_game = select(GameModel).options(selectinload(GameModel.runner)).options(selectinload(GameModel.tournament)).options(selectinload(GameModel.left_team)).options(selectinload(GameModel.right_team)).where(GameModel.id == json.game_id)
+            # self.logger.debug("Retrieving game details...")
+            stmt_game = select(GameModel).options(
+                selectinload(GameModel.runner),
+                selectinload(GameModel.tournament),
+                selectinload(GameModel.left_team),
+                selectinload(GameModel.right_team)
+            ).where(GameModel.id == json.game_id)
             result_game = await self.db_session.execute(stmt_game)
             game = result_game.scalars().first()
             if not game:
@@ -127,14 +154,15 @@ class RunnerManager:
                 return ResponseMessage(success=False, error="Game not found")
 
             # Update runner
+            # self.logger.debug("Updating runner with the new game...")
             runner.games.append(game)
 
             # Start the game
+            # self.logger.debug("Setting game status to RUNNING and assigning port...")
             game.status = GameStatusEnum.RUNNING
             game.start_time = datetime.utcnow()
             game.port = json.port
             game.runner = runner
-
 
             # Commit Changes
             await self.db_session.commit()
@@ -153,6 +181,7 @@ class RunnerManager:
         self.logger.info(f"handle_game_finished: {json}")
         try:
             # Runner
+            # self.logger.debug("Retrieving runner details...")
             stmt_runner = select(RunnerModel).options(selectinload(RunnerModel.games)).where(RunnerModel.id == json.runner_id)
             result_runner = await self.db_session.execute(stmt_runner)
             runner = result_runner.scalars().first()
@@ -162,7 +191,13 @@ class RunnerManager:
                 return ResponseMessage(success=False, error="Runner not found")
 
             # Game
-            stmt_game = select(GameModel).options(selectinload(GameModel.runner)).options(selectinload(GameModel.tournament)).options(selectinload(GameModel.left_team)).options(selectinload(GameModel.right_team)).where(GameModel.id == json.game_id)
+            # self.logger.debug("Retrieving game details...")
+            stmt_game = select(GameModel).options(
+                selectinload(GameModel.runner),
+                selectinload(GameModel.tournament),
+                selectinload(GameModel.left_team),
+                selectinload(GameModel.right_team)
+            ).where(GameModel.id == json.game_id)
             result_game = await self.db_session.execute(stmt_game)
             game = result_game.scalars().first()
 
@@ -171,20 +206,27 @@ class RunnerManager:
                 return ResponseMessage(success=False, error="Game not found")
 
             # Finish the game
+            # self.logger.debug("Updating game status to FINISHED and recording scores...")
             game.status = GameStatusEnum.FINISHED
             game.left_score = json.left_score
             game.right_score = json.right_score
             game.end_time = datetime.utcnow()
             
             # Remove game from runner
+            # self.logger.debug("Removing game from runner's active games...")
             runner.games.remove(game)
 
             # Update Tournament
-            # Check if all games in the tournament are finished
-            stmt_tournament = select(TournamentModel).options(selectinload(TournamentModel.owner)).options(selectinload(TournamentModel.games)).options(selectinload(TournamentModel.teams)).where(TournamentModel.id == game.tournament.id)
+            # self.logger.debug("Checking if all games in the tournament are finished...")
+            stmt_tournament = select(TournamentModel).options(
+                selectinload(TournamentModel.owner),
+                selectinload(TournamentModel.games),
+                selectinload(TournamentModel.teams)
+            ).where(TournamentModel.id == game.tournament.id)
             result_tournament = await self.db_session.execute(stmt_tournament)
             tournament = result_tournament.scalars().first()
             if all(g.status == GameStatusEnum.FINISHED for g in tournament.games):
+                # self.logger.debug("All games finished. Updating tournament status to FINISHED...")
                 tournament.done = True
                 tournament.status = TournamentStatus.FINISHED
 
@@ -206,6 +248,7 @@ class RunnerManager:
         address = f"{json.ip}:{json.port}"
         try:
             # Check if a runner with the same address already exists
+            self.logger.debug("Checking for existing runner with the same address...")
             stmt = select(RunnerModel).options(selectinload(RunnerModel.games)).where(RunnerModel.address == address)
             result = await self.db_session.execute(stmt)
             existing_runner = result.scalars().first()
@@ -227,6 +270,7 @@ class RunnerManager:
                 self.logger.info(f"No existing runner with address {address}. Creating a new runner.")
 
                 # Create a new runner
+                self.logger.debug("Creating a new RunnerModel instance...")
                 new_runner = RunnerModel(
                     status=RunnerStatusMessageEnum.RUNNING,  # Set status to RUNNING upon registration
                     address=address,
@@ -248,11 +292,14 @@ class RunnerManager:
             self.logger.error(f"Unexpected error in register: {e}")
             return ResponseMessage(success=False, error=str(e))
 
-    async def send_command_to_runners(self, runner_ids: List[int], command: RunnerCommandMessageEnum) -> List[Dict[str, Any]]: # A list of responses from each runner.
+    async def send_command_to_runners(self, runner_ids: List[int], command: RunnerCommandMessageEnum) -> List[Dict[str, Any]]:
         self.logger.info(f"send_command_to_runners: Sending command '{command}' to runners: {runner_ids}")
         try:
             if not runner_ids or len(runner_ids) == 0:
-                stmt = select(RunnerModel).where(RunnerModel.status != RunnerStatusMessageEnum.CRASHED).where(RunnerModel.status != RunnerStatusMessageEnum.STOPPED) # not crashed or stopped
+                self.logger.debug("No runner IDs provided. Fetching all active runners...")
+                stmt = select(RunnerModel).where(
+                    RunnerModel.status.notin_([RunnerStatusMessageEnum.CRASHED, RunnerStatusMessageEnum.STOPPED])
+                )
                 result = await self.db_session.execute(stmt)
                 runners = result.scalars().all()
 
@@ -261,10 +308,11 @@ class RunnerManager:
                     return []
                 
                 runner_ids = [runner.id for runner in runners]
-                self.logger.info(f"send_command_to_runners: No runner IDs provided. Using all available runners: {runner_ids}")
+                self.logger.info(f"send_command_to_runners: Using all available runners: {runner_ids}")
 
             responses = []
             for runner_id in runner_ids:
+                self.logger.debug(f"Sending command to Runner ID {runner_id}...")
                 response = await self.send_command(runner_id, command)
                 responses.append({"runner_id": runner_id, "response": response})
             return responses
@@ -273,38 +321,44 @@ class RunnerManager:
             traceback.print_exc()
             return []
                 
-    async def send_command(self, runner_id: int, command: RunnerCommandMessageEnum) -> ResponseMessage:#, command_type: Optional[RunnerCommandTypeEnum] = None, parameters: Optional[Dict[str, str]] = None) -> ResponseMessage:
-        self.logger.info(f"send_command: Sending command '{command}' to runner {runner_id} ") # with type '{command_type}' and parameters {parameters}")
+    async def send_command(self, runner_id: int, command: RunnerCommandMessageEnum) -> ResponseMessage:
+        self.logger.info(f"send_command: Sending command '{command}' to runner {runner_id}")
         try:
             # Retrieve the runner
-            runner_response = await self.get_runner(runner_id)
-            if isinstance(runner_response, ResponseMessage) and not runner_response.success:
+            runner = await self.get_runner_model(runner_id)
+            if not runner:
                 self.logger.error(f"send_command: Runner with id {runner_id} not found")
                 return ResponseMessage(success=False, error="Runner not found")
             
-            runner: RunnerModel = runner_response 
-            
-            # check if the runner is stopped or crashed
-            if runner.status == RunnerStatusMessageEnum.STOPPED or runner.status == RunnerStatusMessageEnum.CRASHED:
+            # Check if the runner is stopped or crashed
+            if runner.status in [RunnerStatusMessageEnum.STOPPED, RunnerStatusMessageEnum.CRASHED]:
                 self.logger.error(f"send_command: Runner with id {runner_id} is stopped or crashed.")
                 return ResponseMessage(success=False, error="Runner is stopped or crashed.")
-            
-            ip=runner.address.split(":")[0]
-            port=runner.address.split(":")[1]
+
+            try:
+                ip, port = runner.address.split(":")
+            except ValueError:
+                self.logger.error(f"send_command: Invalid runner address format for runner {runner_id}: {runner.address}")
+                return ResponseMessage(success=False, error="Invalid runner address format.")
+
             RUNNER_API_KEY = "api-key"  # TODO: get from environment variable or configuration file // os.getenv("RUNNER_API_KEY")
+            message_sender = MessageSender(ip, port, RUNNER_API_KEY)
 
             # Prepare the command data
-            coommand = RequestedCommandToRunnerMessage(command=command)
+            command_data = RequestedCommandToRunnerMessage(command=command)
 
-            message_sender = MessageSender(ip, port, RUNNER_API_KEY)
-            resp = await message_sender.send_message("runner/receive_command", coommand.model_dump())
+            self.logger.debug(f"Sending '{command_data}' to the runner...")
+            # Send the command
+            resp = await message_sender.send_message("runner/receive_command", command_data.model_dump())
             
             if resp.status_code == 200:
                 response_data = resp.json()
                 if response_data.get("success"):
-                    self.logger.info(f"send_command: Command '{command}' successfully sent to runner {runner_id}")
-                    runner.requested_command = command # todo it does not work
+                    self.logger.info(f"Command '{command}' successfully sent to runner {runner_id}")
+                    self.logger.debug("Updating runner's requested_command in the database...")
+                    runner.requested_command = command
                     await self.db_session.commit()
+                    self.logger.info(f"Runner {runner_id} requested_command updated to '{command}'")
                     res = ResponseMessage(
                             success=response_data.get("success"),
                             error=response_data.get("error"),
@@ -317,10 +371,9 @@ class RunnerManager:
                     self.logger.error(f"send_command: Runner responded with error: {error_message}")
                     return ResponseMessage(success=False, error=error_message, value=value_message)
             else:
-                error_message = f"Runner returned status code {resp.status}"
-                # value_message = resp.get("value", None)
+                error_message = f"Runner returned status code {resp.status_code}"
                 self.logger.error(f"send_command: {error_message}")
-                return ResponseMessage(success=False, error=error_message) #, value=value_message)
+                return ResponseMessage(success=False, error=error_message)
         except Exception as e:
             self.logger.error(f"send_command: Unexpected error: {e}")
             traceback.print_exc()
@@ -329,7 +382,6 @@ class RunnerManager:
     async def handle_status_update(self, status_message: RunnerStatusMessage) -> ResponseMessage:
         self.logger.info(f"Handling status update for Runner ID {status_message.runner_id}: {status_message.status}")
         try:
-
             stmt = select(RunnerModel).where(RunnerModel.id == status_message.runner_id)
             result = await self.db_session.execute(stmt)
             runner = result.scalars().first()
@@ -338,19 +390,22 @@ class RunnerManager:
                 self.logger.error(f"Runner with ID {status_message.runner_id} not found.")
                 return ResponseMessage(success=False, error="Runner not found.")
 
-            # Check if the status is needs changing
+            # Check if the status needs changing
             if runner.status == status_message.status:
                 self.logger.info(f"Runner ID {runner.id} is already in status '{runner.status}'. No update needed.")
                 return ResponseMessage(success=True, value="Status is already up-to-date.", error=None)
 
+            self.logger.debug("Updating runner status...")
             runner.status = RunnerStatusMessageEnum(status_message.status)
             runner.last_updated = datetime.utcnow()
             runner.requested_command = RunnerCommandMessageEnum.NONE
+            self.logger.info(f"Runner ID {runner.id} status updated to '{runner.status}'.")
+            self.logger.info(f"Runner ID {runner.id} requested_command reset to '{runner.requested_command}'.")
 
             # Commit the transaction
             await self.db_session.commit()
 
-            self.logger.info(f"Runner ID {runner.id} status updated to '{runner.status}' successfully.")
+            self.logger.info(f"Runner ID {runner.id} status updated successfully.")
             return ResponseMessage(success=True, value="Runner status updated successfully.", error=None)
         
         except SQLAlchemyError as e:
